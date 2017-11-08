038b0006aaeae923f3dff75a58aa03d2b6291c16
Alexander Russell (ajr546) <alexander.james@usask.ca>
2017-11-08 14:05:49 -0600

    Changed project build
    Removed unnecessary files

--- CMPT436Project/Assets/Scripts/touchMove.cs
+++ CMPT436Project/Assets/Scripts/touchMove.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class touchMove : MonoBehaviour {
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+public class ExampleClass : MonoBehaviour
+{
+	public float speed = 0.1F;
+	void Update()
+	{
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+		{
+			// Get movement of the finger since last frame
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 
-	// Update is called once per frame
-	void Update () {
-		
+			// Move object across XY plane
+			transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+		}
 	}
 }
