7a040da6bf7cf1a76877595e79e30cf55b7fb7cc
Alexander Russell (ajr546) <alexander.james@usask.ca>
2017-11-08 16:16:21 -0600

    Added keyboard movement, error in touch movement

--- CMPT436Project/Assets/Scripts/touchMove.cs
+++ CMPT436Project/Assets/Scripts/touchMove.cs
@@ -1,18 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ExampleClass : MonoBehaviour
+public class touchMove : MonoBehaviour
 {
 	public float speed = 0.1F;
+	Rigidbody2D r_body;
+
+	void Start(){
+		r_body = gameObject.GetComponent<Rigidbody2D> ();
+	}
 	void Update()
 	{
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-		{
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase != TouchPhase.Ended) {
 			// Get movement of the finger since last frame
-			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			Vector3 movePosition = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position) - transform.position;
+			movePosition.Normalize ();
+
 
 			// Move object across XY plane
-			transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+			transform.Translate (movePosition*speed);
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate(new Vector3(0,speed,0));
+		}else if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate(new Vector3(0,-speed,0));
+		}else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate(new Vector3(speed,0,0));
+		}else if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate(new Vector3(-speed,0,0));
 		}
 	}
 }
