777a7b153d46563dd8b5832d89fbd8b26f1f591c
Shane Williamson <williamson.shane.a@gmail.com>
2017-11-25 16:22:53 -0600

    Update movement

--- Bomberboy/Assets/Standard Assets/CrossPlatformInput/Scripts/TouchMove.cs
+++ Bomberboy/Assets/Standard Assets/CrossPlatformInput/Scripts/TouchMove.cs
@@ -1,84 +1,125 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.Networking;
 
 public class TouchMove : NetworkBehaviour
 {
-	[SerializeField]
-	private float speed;
+    private const string VERTICAL_KEY = "Vertical";
+    private const string HORIZONTAL_KEY = "Horizontal";
 
-	[SerializeField]
-	private Animator animator;
-	Rigidbody2D r_body;
-
-	Vector2 RIGHT;
-	Vector2 LEFT;
-	Vector2 UP;
-	Vector2 DOWN;
-	Vector2 STOP;
-
-
-	void Start(){
-		r_body = gameObject.GetComponent<Rigidbody2D> ();
-		speed = 2f;
-		animator = gameObject.GetComponent<Animator> ();
+    [SerializeField]
+    private float moveForce = 10;
+    [SerializeField]
+    private float speed = 1;
+    [SerializeField]
+    private bool speedInstantStop = false;
+    [SerializeField]
+    private bool boolForceMove = true;
+    [SerializeField]
+    private Animator animator;
 
 
-		UP = new Vector2 (0, 1);
-		DOWN = new Vector2 (0, -1);
-		RIGHT = new Vector2 (1, 0);
-		LEFT = new Vector2 (-1, 0);
-		STOP = new Vector2 (0, 0);
-	}
+    private Dictionary<string, int> controlState;
+    private Transform creatureTransform;
+    private Rigidbody2D creatureBody;
 
-	public void Move(Vector2 v){
-		if (this.isLocalPlayer) {
-			r_body.velocity = v * speed;
-		}
-	}
+    void Start() {
+        creatureBody = this.GetComponent<Rigidbody2D>();
+        creatureTransform = this.GetComponent<Transform>();
 
-	void FixedUpdate()
-	{
-		
-		Vector2 currentVelocity = r_body.velocity;
+        this.controlState = new Dictionary<string, int>();
+        // Initialize for the case where the player's already holding down keys.
+        this.controlState.Add(VERTICAL_KEY, 0);
+        this.controlState.Add(HORIZONTAL_KEY, 0);
+        this.animator = this.gameObject.GetComponent<Animator>();
+    }
 
+    void Update() {
+        // Update the state of the controls 
+        UpdateMovementState();
+        Debug.Log(Input.GetAxis("Vertical"));
+        float x = this.controlState[HORIZONTAL_KEY];
+        float y = this.controlState[VERTICAL_KEY];
+        //Move(new Vector2(x, y)); // uncomment if using keyboard
+    }
 
-		// Get movement input
-		// Touch input
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			Move (UP);
-		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			Move (DOWN);
-		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			Move (RIGHT);
-		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			Move (LEFT);
-		} else if (Input.GetKeyUp (KeyCode.UpArrow)) {
-			Move (STOP);
-		} else if (Input.GetKeyUp (KeyCode.DownArrow)) {
-			Move (STOP);
-		} else if (Input.GetKeyUp (KeyCode.RightArrow)) {
-			Move (STOP);
-		} else if (Input.GetKeyUp (KeyCode.LeftArrow)) {
-			Move (STOP);
-		}
+    void UpdateMovementState() {
+        // Update the controlState dictionary to represent the state of the movement being pressed
+        int verticalDirection = 0;
+        int horizontalDirection = 0;
+        // Vertical
+        verticalDirection += Input.GetKeyDown(KeyCode.W) ? 1 : 0;
+        verticalDirection += Input.GetKeyUp(KeyCode.W) ? -1 : 0;
+        verticalDirection += Input.GetKeyDown(KeyCode.S) ? -1 : 0;
+        verticalDirection += Input.GetKeyUp(KeyCode.S) ? 1 : 0;
+        // Horizontal
+        horizontalDirection += Input.GetKeyDown(KeyCode.D) ? 1 : 0;
+        horizontalDirection += Input.GetKeyUp(KeyCode.D) ? -1 : 0;
+        horizontalDirection += Input.GetKeyDown(KeyCode.A) ? -1 : 0;
+        horizontalDirection += Input.GetKeyUp(KeyCode.A) ? 1 : 0;
+        // Update the state of the controls
+        this.controlState[VERTICAL_KEY] += verticalDirection;
+        this.controlState[HORIZONTAL_KEY] += horizontalDirection;
+    }
 
-		float deadzone = 0.1f;
+    public void Move(Vector2 myVec) {
+        if (boolForceMove) {
+            ForceMoveVertical(myVec.y);
+            ForceMoveHorizontal(myVec.x);
+        }
+        else {
+            MoveVertical(myVec.y);
+            MoveHorizontal(myVec.x);
+        }
+    }
 
-		if (currentVelocity.x < -deadzone) {
-			animator.SetInteger ("DirectionX", -1);
-		} else if (currentVelocity.x > deadzone) {
-			animator.SetInteger ("DirectionX", 1);
-		} else {
-			animator.SetInteger ("DirectionX", 0);
-		}
+    //apply force to the character to make them move left or right
+    void ForceMoveHorizontal(float x) {
+        creatureBody.AddForce(creatureTransform.right * x * moveForce);
+    }
 
-		if (currentVelocity.y < -deadzone) {
-			animator.SetInteger ("DirectionY", -1);
-		} else if (currentVelocity.y > deadzone) {
-			animator.SetInteger ("DirectionY", 1);
-		} else {
-			animator.SetInteger ("DirectionY", 0);
-		}
-	}
+    //apply force to the character to make the move up or down
+    void ForceMoveVertical(float y) {
+        creatureBody.AddForce(creatureTransform.up * y * moveForce);
+
+    }
+
+    //set speed of horizontal axis of the character to make them move left or right
+    void MoveHorizontal(float x) {
+        if (x != 0 || speedInstantStop) {
+            float cury = creatureBody.velocity.y;
+            float curx = x * speed;
+            creatureBody.velocity = new Vector2(curx, cury);
+            if (x < 0) {
+                animator.SetInteger("DirectionX", -1);
+            } else if (x > 0) {
+                animator.SetInteger("DirectionX", 1);
+            }
+        }
+        if (x == 0) {
+            animator.SetInteger("DirectionX", 0);
+            creatureBody.velocity.Set(0, creatureBody.velocity.y);
+        }
+
+    }
+
+    //set speed of vertical axis of the character to make the move up or down
+    void MoveVertical(float y) {
+        if (y != 0 || speedInstantStop) {
+            float cury = y * speed;
+            float curx = creatureBody.velocity.x;
+            creatureBody.velocity = new Vector2(curx, cury);
+            if (y < 0) {
+                animator.SetInteger("DirectionY", -1);
+            }
+            else if (y > 0) {
+                animator.SetInteger("DirectionY", 1);
+            }
+        }
+        if (y == 0) {
+            animator.SetInteger("DirectionY", 0);
+            creatureBody.velocity.Set(creatureBody.velocity.x, 0);
+        }
+    }
 }
