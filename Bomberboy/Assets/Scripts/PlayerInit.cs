49fc1081b424e393b191e6a9857352b1af6f0f9a
Shane Williamson <williamson.shane.a@gmail.com>
2017-11-25 16:50:45 -0600

    Add player init script

---
+++ Bomberboy/Assets/Scripts/PlayerInit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerInit : NetworkBehaviour {
+	
+    // do da stuff
+	void Start () {
+		if (this.isLocalPlayer) {
+            this.gameObject.GetComponent<TouchMove>().enabled = true;
+            this.gameObject.GetComponent<BombScript>().enabled = true;
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+	}
+}
