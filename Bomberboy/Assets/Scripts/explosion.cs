d644776ad53539f4b5d02487e509df375243dd8c
ConnorNG <netco_nessa@hotmail.com>
2017-12-02 14:40:12 -0600

    added explosion script]

---
+++ Bomberboy/Assets/Scripts/explosion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class explosion : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        Debug.Log(coll.tag);
+        Debug.Log("here");
+        if (coll.tag.Equals("Character") && !coll.gameObject.GetComponent<PlayerStat>().invuln) {
+            coll.gameObject.GetComponent<PlayerStat>().TakeDamage();
+        } else if (coll.tag.Equals("Bomb")) {
+            coll.gameObject.GetComponent<BombBoom>().CmdExplode();
+
+        }
+    }
+}
